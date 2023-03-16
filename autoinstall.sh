47bb32fc660ed9ceb65329ac8004f5a8430f761a
Starkad <ricardolopez010@gmail.com>
2023-03-16 12:45:36 -0500

    Add files via upload

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
+#AlertingProject => https://github.com/ricardolopez010/alerting-ssh-logins-to-mail
+
 
 #----------------------------------------------------
+
 smtpuser="[smtp.gmail.com]:587 cuentadeorigen@gmail.com:contraseña"
-destionodealertas="cuentadedestion@sysadmin.com"
+destinationmail="cuentadedestion@sysadmin.com"
 
 #----------------------------------------------------
 
-#Instalacion de paquetes
+#Install requeried packages
 sudo apt update -y
 sudo apt install postfix libsasl2-modules mailutils -y
 
-#borrado de archivo anterior si ya existe y Creacion del nuevo archivo 
+#delete actual config file if exist, and create the new configuracion file
 rm -r /etc/postfix/sasl/sasl_passwd
 echo $smtpuser >> /etc/postfix/sasl/sasl_passwd
 
-#asignacion de permisos
+#Assign permissions
 sudo postmap /etc/postfix/sasl/sasl_passwd
 sudo chmod 0600 /etc/postfix/sasl/sasl_passwd
 sudo chmod 0600 /etc/postfix/sasl/sasl_passwd.db
 
-#configuracion de archivo main.cf
+#delete actual config file if exist, and create the new configuracion main.cf file
 rm -r /etc/postfix/main.cf
 echo "#Control" >> /etc/postfix/main.cf
 echo "smtpd_banner = $myhostname ESMTP $mail_name (Ubuntu)" >> /etc/postfix/main.cf
@@ -35,17 +38,17 @@ echo "smtp_sasl_password_maps = hash:/etc/postfix/sasl/sasl_passwd" >> /etc/post
 echo "smtp_tls_security_level = encrypt" >> /etc/postfix/main.cf
 echo "smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt" >> /etc/postfix/main.cf
 
-#Para finalizar la configuración, reiniciamos el servicio
+#Restart the postfix service to apply configuration
 
 sudo systemctl restart postfix
 sudo systemctl enable postfix
 
-#Adicion de regla de lectura de login ssh y activacion de la alerta
 
+#Add reading rule to identify ssh logins, and send the mail 
 
 echo "#Alerta-SSH-to-mail" >> /etc/profile
 echo 'if [ -n "$SSH_CLIENT" ]; then' >> /etc/profile
 echo 'TEXT="$(date): ssh login to ${USER}@$(hostname -f)"' >> /etc/profile
 echo 'TEXT="$TEXT from $(echo $SSH_CLIENT|awk '{print $1}')"' >> /etc/profile
-echo 'echo $TEXT|mail -s "Alert SSH login"' "$destionodealertas" >> /etc/profile
+echo 'echo $TEXT|mail -s "Alert SSH login"' "$destinationmail" >> /etc/profile
 echo 'fi' >> /etc/profile
